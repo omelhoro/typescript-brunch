bd5cbfcf40272faa9690b7e2df09b153be6064da
omelhoro <igor.fischer154@gmail.com>
2015-02-07 17:41:16 +0100

    Rewritten algorhithm to sort out .d.ts files and node uses OS-tempdir now

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@
 
     //var TypeScript = require("typescript");
     // var TypeScriptAPI = require("typescript.api");
-    var tsc=require("typescript-compiler");
+    var tsc = require("typescript-compiler");
     var sysPath = require("path");
+    var os = require("os")
 
-    function show_diagnostics (units, callback) {
+    function show_diagnostics(units, callback) {
 
-        for(var n in units) {
-            for(var m in units[n].diagnostics) {
+        for (var n in units) {
+            for (var m in units[n].diagnostics) {
                 var errorMsg = units[n].diagnostics[m].toString();
                 callback(errorMsg, null);
             }
         }
     }
 
-    module.exports = TypeScriptCompiler = (function () {
-        
+    module.exports = TypeScriptCompiler = (function() {
+
         TypeScriptCompiler.prototype.brunchPlugin = true;
         TypeScriptCompiler.prototype.type = "javascript";
         TypeScriptCompiler.prototype.extension = "ts";
@@ -26,66 +27,45 @@
             this.config = config;
             //this.libPath = sysPath.join(__dirname, "..", "node_modules", "typescript", "bin", "lib.d.ts");
         }
-        
-        TypeScriptCompiler.prototype.compile = function (data, path, callback) {
+
+        TypeScriptCompiler.prototype.compile = function(data, path, callback) {
 
             var js = "";
             var error = null;
             var outputWriter = {
-                Write: function (str) {
+                Write: function(str) {
                     js += str;
                 },
-                WriteLine: function (str) {
+                WriteLine: function(str) {
                     js += str + "\n";
                 },
-                Close: function () {
-                }
+                Close: function() {}
             };
             var nullWriter = {
-                Write: function (str) {
-                },
-                WriteLine: function (str) {
-                },
-                Close: function () {
-                }
+                Write: function(str) {},
+                WriteLine: function(str) {},
+                Close: function() {}
             };
 
             path = path.replace(/\\/g, "/");
+            var tmpDir = os.tmpdir()
 
-            try  {
+            function compile() {
                 var error
-                var res=tsc.compile(path,"--module commonjs --outDir /tmp/",null,function(err){
-                    // console.log(Object.keys(err),err.messageText),
-                    error+=err.messageText
-                    // callback(err.messageText,null)
+                    //compile to temporary dir because tsc-compiler cant resolve references in strings
+                var res = tsc.compile(path, "--module commonjs --outDir " + tmpDir, null, function(err) {
+                    error += err.messageText
                 })
-                var fl=Object.keys(res.sources)[0]
-                var comp=res.sources[fl]
-                // console.log(comp)
-                js+=comp
-                // return callback(null,{data: comp })
-                // TypeScriptAPI.resolve([path], function(resolved) {
-                //     if (TypeScriptAPI.check(resolved)) {
-                //         TypeScriptAPI.compile(resolved, function(compiled) {
-                //             if(!TypeScriptAPI.check(compiled)) {
-                //                 show_diagnostics (compiled, callback);
-                //             }
-                //             else {
-                //                 for (var compileUnit in compiled) {
-                //                     callback(null, compiled[compileUnit].content);
-                //                 }
-                //             }
-                //         });
-                //     }
-                //     else {
-                //         show_diagnostics(resolved, callback);
-                //     }
-                // });
-            } 
-            catch (err) {
-                // error += err.stack;
+                var fl = Object.keys(res.sources)[0]
+                var comp = res.sources[fl]
+                js += comp
             }
-            finally {
+
+            try {
+                path.slice(-5) === ".d.ts" ? null : compile()
+            } catch (err) {
+                // error += err.stack;
+            } finally {
                 callback(error, js);
             }
         };
