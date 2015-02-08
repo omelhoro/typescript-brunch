7ebf5e2305122b25720aa7ddfb074fe1097b6c95
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-02-08 18:06:16 +0100

    Refacto to enhance error reporting

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 (function() {
 
-    //var TypeScript = require("typescript");
-    // var TypeScriptAPI = require("typescript.api");
     var tsc = require("typescript-compiler");
     var sysPath = require("path");
-    var os = require("os")
-
-    function show_diagnostics(units, callback) {
-
-        for (var n in units) {
-            for (var m in units[n].diagnostics) {
-                var errorMsg = units[n].diagnostics[m].toString();
-                callback(errorMsg, null);
-            }
-        }
-    }
+    var os = require("os");
+    var Promise = require('promise');
 
     module.exports = TypeScriptCompiler = (function() {
 
@@ -23,51 +12,33 @@
         TypeScriptCompiler.prototype.extension = "ts";
 
         function TypeScriptCompiler(config) {
-
             this.config = config;
-            //this.libPath = sysPath.join(__dirname, "..", "node_modules", "typescript", "bin", "lib.d.ts");
         }
 
         TypeScriptCompiler.prototype.compile = function(data, path, callback) {
-
             var js = "";
-            var error = null;
-            var outputWriter = {
-                Write: function(str) {
-                    js += str;
-                },
-                WriteLine: function(str) {
-                    js += str + "\n";
-                },
-                Close: function() {}
-            };
-            var nullWriter = {
-                Write: function(str) {},
-                WriteLine: function(str) {},
-                Close: function() {}
-            };
-
+            var tmpDir = os.tmpdir();
             path = path.replace(/\\/g, "/");
-            var tmpDir = os.tmpdir()
 
-            function compile() {
-                var error
+            var promise = new Promise(function (resolve, reject) {
+                if (path.slice(-5) !== ".d.ts") {
                     //compile to temporary dir because tsc-compiler cant resolve references in strings
-                var res = tsc.compile(path, "--module commonjs --outDir " + tmpDir, null, function(err) {
-                    error += err.messageText
-                })
-                var fl = Object.keys(res.sources)[0]
-                var comp = res.sources[fl]
-                js += comp
-            }
+                    var res = tsc.compile(path, "--module commonjs --outDir " + tmpDir, null, function(err) {
+                        var error = err.formattedMessage + '\n';
+                        reject(error);
+                    });
+                    var fl = Object.keys(res.sources)[0];
+                    var comp = res.sources[fl];
+                    js += comp;
+                    resolve(js);
+                }
+            });
 
-            try {
-                path.slice(-5) === ".d.ts" ? null : compile()
-            } catch (err) {
-                // error += err.stack;
-            } finally {
-                callback(error, js);
-            }
+            promise.then(function(result) {
+                callback(null, result);
+            }, function(error) {
+                callback(error);
+            })
         };
         return TypeScriptCompiler;
     })();
