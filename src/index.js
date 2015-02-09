56feaa882e50ee34ea39433a30e8b25d6bc8411d
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-02-09 13:11:25 +0100

    Change function wrapper + compilated code is well related to its file

--- src/index.js
+++ src/index.js
@@ -5,41 +5,45 @@
     var os = require("os");
     var Promise = require('promise');
 
-    module.exports = TypeScriptCompiler = (function() {
+    function TypeScriptCompiler(config) {
+        this.config = config;
+    }
 
-        TypeScriptCompiler.prototype.brunchPlugin = true;
-        TypeScriptCompiler.prototype.type = "javascript";
-        TypeScriptCompiler.prototype.extension = "ts";
+    TypeScriptCompiler.prototype.brunchPlugin = true;
+    TypeScriptCompiler.prototype.type = "javascript";
+    TypeScriptCompiler.prototype.extension = "ts";
 
-        function TypeScriptCompiler(config) {
-            this.config = config;
-        }
+    TypeScriptCompiler.prototype.compile = function(data, path, callback) {
+        var js = "";
+        var tmpDir = os.tmpDir();
+        path = path.replace(/\\/g, "/");
 
-        TypeScriptCompiler.prototype.compile = function(data, path, callback) {
-            var js = "";
-            var tmpDir = os.tmpdir();
-            path = path.replace(/\\/g, "/");
-
-            var promise = new Promise(function (resolve, reject) {
-                if (path.slice(-5) !== ".d.ts") {
-                    //compile to temporary dir because tsc-compiler cant resolve references in strings
-                    var result = tsc.compile(path, "--module commonjs --outDir " + tmpDir, null, function(err) {
-                        var error = err.formattedMessage + '\n';
-                        reject(error);
-                    });
-                    var fileName = Object.keys(result.sources)[0];
-                    var comp = result.sources[fileName];
-                    js += comp;
-                    resolve(js);
+        var promise = new Promise(function (resolve, reject) {
+            if (path.slice(-5) !== ".d.ts") {
+                //compile to temporary dir because tsc-compiler cant resolve references in strings
+                var result = tsc.compile(path, "--module commonjs --outDir " + tmpDir, null, function(err) {
+                    var error = err.formattedMessage + '\n';
+                    reject(error);
+                });
+                var fileName = null;
+                var sourceFiles = Object.keys(result.sources);
+                for(var i = 0; i < sourceFiles.length; i++) {
+                    if (sysPath.basename(sourceFiles[i], '.js') == sysPath.basename(path, '.ts')) {
+                        fileName = sourceFiles[i];
+                    }
                 }
-            });
+                var comp = result.sources[fileName];
+                js += comp;
+                resolve(js);
+            }
+        });
 
-            promise.then(function(result) {
-                callback(null, result);
-            }, function(error) {
-                callback(error);
-            })
-        };
-        return TypeScriptCompiler;
-    })();
+        promise.then(function(result) {
+            callback(null, result);
+        }, function(error) {
+            callback(error);
+        })
+    };
+
+    module.exports = TypeScriptCompiler;
 }).call(this);
